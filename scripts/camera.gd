0d704086c7ec173f455b655070489c23bf0ecf59
Schwarzer-Tee <elbrecht.anton@gmail.com>
2024-07-26 01:59:36 +0200

    first version
    first version

---
+++ scripts/camera.gd
@@ -0,0 +1,59 @@
+extends CharacterBody3D
+
+
+var SPEED = 25.0
+const JUMP_VELOCITY = 4.5
+@onready var cam_tex: TextEdit = $"../Gui/VBoxContainer/camera/cam-tex"
+
+@onready var head: CharacterBody3D = $"."
+@onready var camera: Camera3D = $Camera3D
+var SENSITIVITY = 0.01
+func _unhandled_input(event):
+	if event is InputEventMouseButton:
+		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	elif event.is_action_pressed("ui_cancel"):
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
+		if event is InputEventMouseMotion:
+			head.rotate_y(-event.relative.x * SENSITIVITY)
+			camera.rotate_x(-event.relative.y * SENSITIVITY)
+			camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-90), deg_to_rad(90))
+		
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	var multi = float(cam_tex.text)
+	SPEED = 25 * multi
+	if not is_on_floor():
+		#velocity += get_gravity() * delta
+		pass
+	if Input.is_action_pressed("space"):
+		velocity -= get_gravity() * multi * delta
+	elif Input.is_action_pressed("shift") == false:
+		velocity.y = 0
+	if Input.is_action_pressed("shift"):
+		velocity += get_gravity() * multi * delta
+	elif Input.is_action_pressed("space") == false:
+		velocity.y = 0
+		
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+
+	move_and_slide()
+
+
+func _on_set_button_down() -> void:
+	$"../Gui/VBoxContainer/camera/cam-tex".focus_mode = false
+	$"../Gui/VBoxContainer/camera/cam-tex".focus_mode = true
+	pass # Replace with function body.
---
+++ scripts/grid_map.gd
@@ -0,0 +1,66 @@
+extends GridMap
+@export var refreshrate = 0.1
+#@export var accuracy = 1
+@export var frequency = 0.2
+@export var depth = 7
+@export var sizex = 50
+@export var sizez = 50
+
+var counter = 0
+@onready var timer: Timer = $"../Timer"
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	$"../Gui/VBoxContainer/depth/depth-tex".text = str(depth)
+	$"../Gui/VBoxContainer/frequency/frequency-tex".text = str(frequency)
+	$"../Gui/VBoxContainer/refreshrate/refreshrate-tex".text = str(timer.wait_time)
+	$"../Gui/VBoxContainer/sizex/sizex-tex".text = str(sizex)
+	$"../Gui/VBoxContainer/sizez/sizez-tex".text = str(sizez)
+	$"../Gui/VBoxContainer/camera/cam-tex".focus_mode = false
+	$"../Gui/VBoxContainer/camera/cam-tex".focus_mode = true
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if Input.is_action_pressed("ui_left") || Input.is_action_pressed("ui_right") || Input.is_action_pressed("ui_up") || Input.is_action_pressed("ui_down") || Input.is_action_pressed("shift") || Input.is_action_pressed("space"):
+		$"../Gui/VBoxContainer/depth/depth-tex".focus_mode = false
+		$"../Gui/VBoxContainer/depth/depth-tex".focus_mode = true
+		$"../Gui/VBoxContainer/frequency/frequency-tex".focus_mode = false
+		$"../Gui/VBoxContainer/frequency/frequency-tex".focus_mode = true
+		$"../Gui/VBoxContainer/refreshrate/refreshrate-tex".focus_mode = false
+		$"../Gui/VBoxContainer/refreshrate/refreshrate-tex".focus_mode = true
+		$"../Gui/VBoxContainer/sizex/sizex-tex".focus_mode = false
+		$"../Gui/VBoxContainer/sizex/sizex-tex".focus_mode = true
+		$"../Gui/VBoxContainer/sizez/sizez-tex".focus_mode = false
+		$"../Gui/VBoxContainer/sizez/sizez-tex".focus_mode = true
+		
+	pass
+func sinwave(count):
+	clear()
+	for x in range(-sizex/2, sizex/2):
+		for z in range(-sizez/2, sizez/2):
+			var sinx = sin((x+count)*frequency) * depth
+			var cosy = cos((z+count)*frequency) * depth
+			sinx = (sinx + cosy) / 2   
+			set_cell_item(Vector3i(x,sinx,z),0)
+			pass
+
+
+func _on_timer_timeout() -> void:
+	sinwave(counter)
+	counter += 1
+	if counter > 9223372036854775800:
+		counter = 0
+	pass # Replace with function body.
+
+
+
+
+func _on_set_button_down() -> void:
+	depth = int($"../Gui/VBoxContainer/depth/depth-tex".text)
+	frequency = float($"../Gui/VBoxContainer/frequency/frequency-tex".text)
+	timer.wait_time = float($"../Gui/VBoxContainer/refreshrate/refreshrate-tex".text)
+	sizex = int($"../Gui/VBoxContainer/sizex/sizex-tex".text)
+	sizez = int($"../Gui/VBoxContainer/sizez/sizez-tex".text)
+	
+	pass # Replace with function body.
